4a3c38a4e1e48c1c71c444b613c367b3cc174096
Alex Muravya <asm@virtalab.net>
2014-01-23 18:09:50 +0400

    Tests added

---
+++ src/test/java/net/virtalab/vson/test/GsonTests.java
@@ -0,0 +1,77 @@
+package net.virtalab.vson.test;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import net.virtalab.vson.test.type.MyJson;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Edit this.
+ */
+public class GsonTests extends Assert {
+    private Gson gson;
+
+    @Before
+    public void prepare(){
+        this.gson = new Gson();
+    }
+
+    @Test
+    public void customStringTest(){
+        //JSE <- IllegalStateE
+        String str = "I am not JSON {";
+        MyJson o = gson.fromJson(str, MyJson.class);
+        assertTrue(true);
+    }
+
+    @Test(expected = JsonSyntaxException.class)
+    public void badSyntaxJsonTest(){
+        //JSE <- MalformedJsonE
+        String str = "{\"key\":1,}";
+        MyJson o = gson.fromJson(str,MyJson.class);
+        assertTrue(true);
+    }
+
+    @Test(expected = JsonSyntaxException.class)
+    public void malformedJsonTest(){
+        //JSE <- MalformedJsonE
+        String str = "{'key\":1}";
+        MyJson o = gson.fromJson(str,MyJson.class);
+        assertTrue(o.getKey()==1);
+    }
+
+    @Test
+    public void emptyJsonTest(){
+        //passed
+        //key - null, value - null
+        String str = "{}";
+        MyJson o = gson.fromJson(str,MyJson.class);
+        assertTrue(o!=null);
+    }
+    @Test
+    public void badObjectImplTest(){
+        //key - 1, value = null
+        String str = "{key:1, bad_field:fake}";
+        MyJson o = gson.fromJson(str,MyJson.class);
+        assertTrue(o.getKey() != 0);
+    }
+
+    @Test
+    public void badTypeAtObjectImplTest(){
+        //JSE <- ISE (norm explain)
+        String str = "{key:ss, value:aaa}";
+        MyJson o = gson.fromJson(str,MyJson.class);
+        assertTrue(o.getKey() != 0);
+    }
+
+    @Test
+    public void nullObjectImplTest(){
+        //key - 1, value = null
+        String str = "{key:1, value:null}";
+        MyJson o = gson.fromJson(str,MyJson.class);
+        assertTrue(o.getValue() == null);
+    }
+
+}
---
+++ src/test/java/net/virtalab/vson/test/IncomingParsingTests.java
@@ -0,0 +1,48 @@
+package net.virtalab.vson.test;
+
+import net.virtalab.vson.Vson;
+import org.junit.Assert;
+import org.junit.Before;
+
+/**
+ * Parser Tests
+ */
+public class IncomingParsingTests extends Assert {
+    private Vson vson;
+
+    @Before
+    public void prepare(){
+        this.vson = new Vson();
+    }
+
+    public void customStringTest(){
+        String str = "I am not a json at all";
+    }
+
+    public void emptyJsonTest(){
+        String str = "{}";
+    }
+    public void badSyntaxJsonTest(){
+        String str = "{'key\":1}";
+    }
+
+    public void wrongObjectImplementationTest(){
+        String str = "{key:1, bad_field:fake}";
+    }
+
+    public void notAllFieldsPresentTest(){
+        String str = "{key:1}";
+    }
+
+    public void fieldsWithNullTest(){
+        String str = "{key:1, value: null }";
+    }
+
+    public void complexObjectTest(){
+
+    }
+
+    public void EcsapeCharactersTest(){
+
+    }
+}
